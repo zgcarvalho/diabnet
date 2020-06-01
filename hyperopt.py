363e645c077c33929847f3411d92d52028717911
Jose Geraldo de Carvalho Pereira <Jose Geraldo>
2020-06-01 16:23:33 -0300

    last model: model-13 2500 epochs after 5 rounds of hyperopt

--- hyperopt.py
+++ hyperopt.py
@@ -5,40 +5,48 @@ import optuna
 
 # DATASET = './datasets/visits_sp_unique_train_positivo_1000_negative_0.csv'
 
+POSITIVE_TRAINSET = None
+RANDOM_TRAINSET = None
+
+def get_trainset(fn):
+    feat = data.get_feature_names(fn, BMI=False, sex=True, parents_diagnostics=True)
+    dataset = data.DiabDataset(fn, feat, random_age=False, soft_label=True)
+    return dataset
+
 def objective(trial):
     params = {
-        "l1_neurons": trial.suggest_categorical('l1_neurons', [3,5,8,13,21,34,55,89,144,233]),
+        "l1_neurons": trial.suggest_categorical('l1_neurons', [8,13,21]),
         "l2_neurons": 0,
         "l3_neurons": 0,
-        "dp0": trial.suggest_discrete_uniform('dp0', 0.05, 0.8, 0.05),
+        "dp0": trial.suggest_discrete_uniform('dp0', 0.35, 0.7, 0.05),
         "dp1": 0.0, # 0.5
         "dp2": 0,
         "dp3": 0,
-        "lr": trial.suggest_loguniform('lr', 0.0001, 0.01),
+        "lr": trial.suggest_uniform('lr', 0.001, 0.007),
         "wd": 0.000001,
-        "lambda1_dim1": trial.suggest_loguniform('lambda1_dim1', 0.00001, 0.001),
-        "lambda2_dim1": trial.suggest_loguniform('lambda2_dim1', 0.001, 0.1),
-        "lambda1_dim2": trial.suggest_loguniform('lambda1_dim2', 0.00001, 0.001),
-        "lambda2_dim2": trial.suggest_loguniform('lambda2_dim2', 0.001, 0.1),
-        "flood_penalty":trial.suggest_uniform('flood_penalty', 0.0, 0.6)
+        "lambda1_dim1": trial.suggest_loguniform('lambda1_dim1', 1e-10, 5e-6),
+        "lambda2_dim1": trial.suggest_loguniform('lambda2_dim1', 0.00001, 0.001),
+        "lambda1_dim2": trial.suggest_loguniform('lambda1_dim2', 1e-10, 5e-6),
+        "lambda2_dim2": trial.suggest_loguniform('lambda2_dim2', 0.00001, 0.01),
+        "flood_penalty":trial.suggest_uniform('flood_penalty', 0.38, 0.41)
     }
     epochs = 2500
 
+    global POSITIVE_TRAINSET
+    global RANDOM_TRAINSET
+
     fn_positive_dataset = './datasets/visits_sp_unique_train_positivo_1000_random_0.csv'
-    fn_random_dataset = './datasets/visits_sp_unique_train_randomized_1_positivo_1000_random_0.csv'
+    if POSITIVE_TRAINSET == None:
+        POSITIVE_TRAINSET = get_trainset(fn_positive_dataset)
 
-    positive_features = data.get_feature_names(fn_positive_dataset, BMI=False, sex=True, parents_diagnostics=True)
-    random_features = data.get_feature_names(fn_random_dataset, BMI=False, sex=True, parents_diagnostics=True)
+    # fn_random_dataset = './datasets/visits_sp_unique_train_randomized_1_positivo_1000_random_0.csv'
+    # if RANDOM_TRAINSET == None:
+    #     RANDOM_TRAINSET = get_trainset(fn_random_dataset)
+    
+    len_trainset = int(0.9*len(POSITIVE_TRAINSET))
+    positive_trainset, positive_valset = random_split(POSITIVE_TRAINSET, [len_trainset, len(POSITIVE_TRAINSET)-len_trainset])
+    # random_trainset, random_valset = random_split(RANDOM_TRAINSET, [len_trainset, len(RANDOM_TRAINSET)-len_trainset])
 
-    positive_dataset = data.DiabDataset(fn_positive_dataset, positive_features, random_age=False, soft_label=True)
-    random_dataset = data.DiabDataset(fn_random_dataset, random_features, random_age=False, soft_label=True)
-
-    len_trainset = int(0.9*len(positive_dataset))
-    positive_trainset, positive_valset = random_split(positive_dataset, [len_trainset, len(positive_dataset)-len_trainset])
-    random_trainset, random_valset = random_split(random_dataset, [len_trainset, len(random_dataset)-len_trainset])
-
-    # fn_out = f"diabnet/models/model-32-sp-soft-label-randomized_1_positives-1000-dropout0-05-bn-decay-flood-{i:03}.pth"
-    # print("model saved to:", fn_out)
     positive_train_loss, positive_val_loss = train(params, positive_trainset, positive_valset, epochs, is_trial=True, device='cuda')
     # random_train_loss, random_val_loss = train(params, random_trainset, random_valset, epochs, is_trial=True, device='cuda')
     # score = positive_train_loss - random_train_loss
--- training.py
+++ training.py
@@ -9,20 +9,20 @@ def net(fn_dataset):
     for i in range(100):
         print(f"Model {i:03}")
         params = {
-            "l1_neurons": 3, # 256
+            "l1_neurons": 13, # 256
             "l2_neurons": 0,
             "l3_neurons": 0,
-            "dp0": 0.75,
+            "dp0": 0.45,
             "dp1": 0.0, # 0.5
             "dp2": 0,
             "dp3": 0,
-            "lr": 0.003,
+            "lr": 0.0045,
             "wd": 0.000001,
-            "lambda1_dim1": 0.0004,
-            "lambda2_dim1": 0.05,
-            "lambda1_dim2": 0.00002,
-            "lambda2_dim2": 0.025,
-            "flood_penalty": 0.25
+            "lambda1_dim1": 0.0000,
+            "lambda2_dim1": 0.000075,
+            "lambda1_dim2": 0.0000,
+            "lambda2_dim2": 0.00005,
+            "flood_penalty": 0.39
         }
 
         epochs = 2500
@@ -35,7 +35,7 @@ def net(fn_dataset):
         len_trainset = int(0.9*len(dataset))
         trainset, valset = random_split(dataset, [len_trainset, len(dataset)-len_trainset])
 
-        fn_out = f"diabnet/models/model-3-sp-soft-label-positives-2500-dropout0-bn-decay-flood-hyperopt-{i:03}.pth"
+        fn_out = f"diabnet/models/model-13-sp-soft-label-randomized-positives-2500-dropout0-bn-decay-flood-hyperopt-{i:03}.pth"
         print("model saved to:", fn_out)
         train(params, trainset, valset, epochs, fn_out, device='cuda')
         # break to train only one model
@@ -43,4 +43,4 @@ def net(fn_dataset):
 
 if __name__ == "__main__":
 
-    net('./datasets/visits_sp_unique_train_positivo_1000_random_0.csv')
+    net('./datasets/visits_sp_unique_train_randomized_0_positivo_1000_random_0.csv')
