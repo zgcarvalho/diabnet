e735ee7c36c124e19eb9442a019a8011505caf49
Jose Geraldo <zgcarvalho@outlook.com>
2020-08-31 16:08:47 -0300

    pre remove apply without ensemble

--- diabnet/apply.py
+++ diabnet/apply.py
@@ -1,102 +1,102 @@
-import pandas as pd
-import numpy as np
-import torch 
-import os
-from diabnet.model import load
-from diabnet.data import encode_features
-# from model import load
+# import pandas as pd
+# import numpy as np
+# import torch 
+# import os
+# from diabnet.model import load
+# from diabnet.data import encode_features
+# # from model import load
 
-class Predictor(object):
-    """
-    Predictor for type 2 diabetes.
-    """
-    def __init__(self, model, feature_names, negatives_csv=None):
-        """
-        Construct a predictor for type 2 diabetes .
+# class Predictor(object):
+#     """
+#     Predictor for type 2 diabetes.
+#     """
+#     def __init__(self, model, feature_names, negatives_csv=None):
+#         """
+#         Construct a predictor for type 2 diabetes .
 
-        Parameters:
-        model(string): path to neural network previously trained and saved, i.e. "diabnet/models/model.pth"
-        feature_names(list(strings)): list with feature names
-        negatives_csv(string): path to csv file with negatives (pacients without diabetes), ie "diabnet/data/negatives_older60.csv"  
-        """
-        # TODO assert len feature_names is equal to model input size
-        self.model = load(os.path.join(os.path.dirname(__file__), model))
-        self.feat_names = feature_names
-        self.negative_data = get_negative_data(negatives_csv, self.feat_names) if negatives_csv != None else None
+#         Parameters:
+#         model(string): path to neural network previously trained and saved, i.e. "diabnet/models/model.pth"
+#         feature_names(list(strings)): list with feature names
+#         negatives_csv(string): path to csv file with negatives (pacients without diabetes), ie "diabnet/data/negatives_older60.csv"  
+#         """
+#         # TODO assert len feature_names is equal to model input size
+#         self.model = load(os.path.join(os.path.dirname(__file__), model))
+#         self.feat_names = feature_names
+#         self.negative_data = get_negative_data(negatives_csv, self.feat_names) if negatives_csv != None else None
 
-    def patient(self, feat, age=-1, bmi=-1, samples=100):
-        """
-        Parameters:
-        feat: patient features snps (0,1,2), age and bmi.
-        age: if age==-1 feat[age] will be used. else age==n n will be used
-        bmi: if bmi==-1 feat[bmi] will be used if it exists. else bmi==n n will be used (this has effect only in NN that use bmi as feature)
-        """
-        if bmi != -1 and 'BMI' not in self.feat_names:
-            print("Warning: BMI is not a feature for this network. Therefore, no impact at predicted values.")
-        # features = encode_features(feat, self.feat_names, age, bmi)
-        # features = encode_features(self.feat_names, feat)
-        # features = torch.unsqueeze(torch.tensor(features, dtype=torch.float), dim=0)
+#     def patient(self, feat, age=-1, bmi=-1, samples=100):
+#         """
+#         Parameters:
+#         feat: patient features snps (0,1,2), age and bmi.
+#         age: if age==-1 feat[age] will be used. else age==n n will be used
+#         bmi: if bmi==-1 feat[bmi] will be used if it exists. else bmi==n n will be used (this has effect only in NN that use bmi as feature)
+#         """
+#         if bmi != -1 and 'BMI' not in self.feat_names:
+#             print("Warning: BMI is not a feature for this network. Therefore, no impact at predicted values.")
+#         # features = encode_features(feat, self.feat_names, age, bmi)
+#         # features = encode_features(self.feat_names, feat)
+#         # features = torch.unsqueeze(torch.tensor(features, dtype=torch.float), dim=0)
         
-        # pass a copy of feature to NOT change original features
-        features = self._encode_features(np.copy(feat), age=age, bmi=bmi)
-        return self._sampler(features, samples)
+#         # pass a copy of feature to NOT change original features
+#         features = self._encode_features(np.copy(feat), age=age, bmi=bmi)
+#         return self._sampler(features, samples)
 
-    def negatives(self, age=-1, bmi=-1, samples=30):
-        if bmi != -1 and 'BMI' not in self.feat_names:
-            print("Warning: BMI is not a feature for this network. Therefore, no impact at predicted values.")
-        if self.negative_data is not None:
-            probs = []
-            for n in self.negative_data:
-                # f = encode_features(n, self.feat_names, age, bmi)
-                f = self._encode_features(n, age=age, bmi=bmi)
-                # f = encode_features(self.feat_names, n)
-                # f = torch.tensor(f, dtype=torch.float)
-                p = self._sampler(f, samples)
-                probs.append(p)
-            return np.concatenate(probs)
-        else:
-            print("No information about negatives")
-            return None 
+#     def negatives(self, age=-1, bmi=-1, samples=30):
+#         if bmi != -1 and 'BMI' not in self.feat_names:
+#             print("Warning: BMI is not a feature for this network. Therefore, no impact at predicted values.")
+#         if self.negative_data is not None:
+#             probs = []
+#             for n in self.negative_data:
+#                 # f = encode_features(n, self.feat_names, age, bmi)
+#                 f = self._encode_features(n, age=age, bmi=bmi)
+#                 # f = encode_features(self.feat_names, n)
+#                 # f = torch.tensor(f, dtype=torch.float)
+#                 p = self._sampler(f, samples)
+#                 probs.append(p)
+#             return np.concatenate(probs)
+#         else:
+#             print("No information about negatives")
+#             return None 
 
-    def patient_life(self, feat, age_range=range(20,90,5), bmi=-1, samples=100):
-        probs_by_age = []
-        for age in age_range:
-            probs_by_age.append(self.patient(feat, age, bmi, samples))
-        return probs_by_age, age_range
+#     def patient_life(self, feat, age_range=range(20,90,5), bmi=-1, samples=100):
+#         probs_by_age = []
+#         for age in age_range:
+#             probs_by_age.append(self.patient(feat, age, bmi, samples))
+#         return probs_by_age, age_range
 
-    def negatives_life(self, age_range=range(20,90,5), bmi=-1, samples=30):
-        if self.negative_data is not None:   
-            probs_by_age = []
-            for age in age_range:
-                probs_by_age.append(self.negatives(age, bmi, samples))
-            return probs_by_age, age_range
-        else:
-            print("No information about negatives")
-            return None
+#     def negatives_life(self, age_range=range(20,90,5), bmi=-1, samples=30):
+#         if self.negative_data is not None:   
+#             probs_by_age = []
+#             for age in age_range:
+#                 probs_by_age.append(self.negatives(age, bmi, samples))
+#             return probs_by_age, age_range
+#         else:
+#             print("No information about negatives")
+#             return None
 
         
-    def _sampler(self, x, n):
-        return np.array([torch.sigmoid(self.model(x)).detach().numpy()[0,0] for i in range(n)])
+#     def _sampler(self, x, n):
+#         return np.array([torch.sigmoid(self.model(x)).detach().numpy()[0,0] for i in range(n)])
 
-    def _encode_features(self, feat, age=-1, bmi=-1):
-        if age >= 0:
-            age_idx = self.feat_names.index('AGE')
-            feat[age_idx] = age
-        if bmi >= 0:
-            bmi_idx = self.feat_names.index('BMI')
-            feat[bmi_idx] = bmi
-        features = encode_features(self.feat_names, feat)
-        features = torch.unsqueeze(torch.tensor(features, dtype=torch.float), dim=0)
-        return features
+#     def _encode_features(self, feat, age=-1, bmi=-1):
+#         if age >= 0:
+#             age_idx = self.feat_names.index('AGE')
+#             feat[age_idx] = age
+#         if bmi >= 0:
+#             bmi_idx = self.feat_names.index('BMI')
+#             feat[bmi_idx] = bmi
+#         features = encode_features(self.feat_names, feat)
+#         features = torch.unsqueeze(torch.tensor(features, dtype=torch.float), dim=0)
+#         return features
 
-def get_negative_data(fn, columns):
-    # script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
-    # abs_file_path = os.path.join(script_dir, fn)
-    print("NEGATIVE FILE", fn)
-    df = pd.read_csv(fn)
-    # feat = df.values[:,2:-1]
-    feat = df[columns].values
-    return feat
+# def get_negative_data(fn, columns):
+#     # script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
+#     # abs_file_path = os.path.join(script_dir, fn)
+#     print("NEGATIVE FILE", fn)
+#     df = pd.read_csv(fn)
+#     # feat = df.values[:,2:-1]
+#     feat = df[columns].values
+#     return feat
 
-if __name__ == "__main__":
-    pass
+# if __name__ == "__main__":
+#     pass
