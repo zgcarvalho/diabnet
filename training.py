1ebda429c3bd69baf252d9b1d6743204dd6f40d8
Jose Geraldo de Carvalho Pereira <Jose Geraldo>
2020-09-07 14:34:55 -0300

    adamw round1

--- training.py
+++ training.py
@@ -10,21 +10,22 @@ def net(fn_dataset):
         # start_time = default_timer()
         print(f"Model {i:03}")
         params = {
-            "hidden_neurons": 6, #13
-            "dropout": 0.51,
-            "lr": 0.027,
-            "beta1": 0.9998,
-            "beta2": 0.9994,
-            "eps": 1.5e-6,
-            "wd": 1.2e-5,
-            "lambda1_dim1": 1e-6,
-            "lambda2_dim1": 3.3e-9,
-            "lambda1_dim2": 2e-7,
-            "lambda2_dim2": 5e-8,
-            "flood_penalty": 0.03, #0.39
-            "soft_label_alpha": 0.05,
-            "batch_size": 256
+            "hidden_neurons": 5, #13
+            "dropout": 0.55,
+            "lr": 0.03,
+            "beta1": 0.999,
+            "beta2": 0.99999,
+            "eps": 1e-6,
+            "wd": 1e-07, 
+            "lambda1_dim1": 5e-07, 
+            "lambda2_dim1": 5e-06,
+            "lambda1_dim2": 5e-08,
+            "lambda2_dim2": 5e-08,
+            "flood_penalty": 0.15,
+            "soft_label_alpha": 0.01,
+            "batch_size": 256  
         }
+        
 
         epochs = 2500
 
@@ -36,7 +37,7 @@ def net(fn_dataset):
         len_trainset = int(0.9*len(dataset))
         trainset, valset = random_split(dataset, [len_trainset, len(dataset)-len_trainset])
 
-        fn_out = f"diabnet/models/model-6-soft-label-age-positives-2500-dropout0-bn-decay-flood-hyperopt-batch256-lc1-20200831-{i:03}.pth"
+        fn_out = f"diabnet/models/model-5-soft-label-age-positives-2500-dropout0-bn-decay-flood-hyperopt-batch256-adamw-lc1-20200907-{i:03}.pth"
         print("model saved to:", fn_out)
         train(params, trainset, valset, epochs, fn_out, device='cuda', is_trial=False)
         # end_time = default_timer()
