fe6a214d8fabc6b4ff87e3182d64651fcd9302bc
jvsguerra <jvsguerra@gmail.com>
2021-07-08 18:58:26 -0300

    Update notebook 7 - Correlation between training and test set results

--- diabnet/analysis/analysis.py
+++ diabnet/analysis/analysis.py
@@ -1,13 +1,15 @@
-import matplotlib
+import torch
 import seaborn
+import seaborn as sns
 import numpy as np
 import pandas as pd
 from typing import Dict, List, Union, Tuple, Optional
 from collections import OrderedDict
-from matplotlib.ticker import AutoMinorLocator
 import matplotlib.pyplot as plt
-import seaborn as sns
+from matplotlib.ticker import AutoMinorLocator
 from sklearn.utils.validation import indexable
+from captum.attr import IntegratedGradients
+from diabnet.data import encode_features
 
 COLORS = sns.color_palette("colorblind")
 
@@ -662,6 +664,7 @@ def plot_precision_recall_comp(r, interval="HDI"):
 # --------------------------------------------------------------------------- #
 # -------------- Notebook 4: Family Cross Validation analysis --------------- #
 
+
 def _data2barplot(
     data: pd.DataFrame, selection: Optional[List[str]] = None, orient: str = "index"
 ) -> Tuple[Dict[str, List[float]], List[str]]:
@@ -822,3 +825,76 @@ def barplot(
     # Draw legend if we need
     if legend:
         ax.legend(bars, data.keys())
+
+
+# --------------------------------------------------------------------------- #
+# ----------------- Notebook 7: Feature importance analysis ----------------- #
+
+
+def get_feature_importance(values, age, sex) -> pd.DataFrame:
+    imp = values.calc_attr(age, sex)
+    # testa se o SNP tem valores 1 ou 2. caso não tenha, sua importancia não pode ser calculada
+    s = {k: [np.mean(imp[k]), np.median(imp[k])] for k in imp if len(imp[k]) > 0}
+    df = pd.DataFrame.from_dict(s, orient="index")
+    df.rename(columns={0: f"{sex}{age}_mean", 1: f"{sex}{age}_median"}, inplace=True)
+
+    return df
+
+
+class ExplainModel:
+    def __init__(self, ensemble, feature_names, dataset_fn):
+        self.ensemble = ensemble
+        self.feat_names = feature_names
+        self.dataset_fn = dataset_fn
+
+    def encode(self, feat, age=50):
+        feat[self.feat_names.index("AGE")] = age
+        t = torch.Tensor(encode_features(self.feat_names, feat))
+        t.requires_grad_()
+        return t
+
+    def gen_baselines(self, sex, age=50):
+        baseline = np.zeros(1004, dtype=object)
+        baseline[1001] = sex  # 'M'|'F'|'X'  'X' no information
+        baseline = self.encode(baseline, age)
+        return baseline
+
+    def gen_inputs(self, feats, age=50):
+        inputs = torch.stack([self.encode(f, age=age) for f in feats])
+        return inputs
+
+    def calc_attr(self, age, sex):
+        df = pd.read_csv(self.dataset_fn)
+        if sex != "X":
+            df = df[df["sex"] == sex]
+        features = df[self.feat_names].values
+        inputs = self.gen_inputs(features, age=age)
+        baseline = torch.stack(
+            [self.gen_baselines(sex, age=age) for i in range(len(inputs))]
+        )  # same dimension as inputs
+
+        # attrs = []
+        imp = {self.feat_names[i]: [] for i in range(1000)}
+        # mask to inputs -> snps 1 and 2 == True / snps 0 == False
+        mask = inputs[:, 1, :1000] > 0.5
+        for m in self.ensemble.models:
+            ig = IntegratedGradients(lambda x: m.apply(x))
+            attr = ig.attribute(inputs, baselines=baseline)
+            for i in range(1000):
+                imp[self.feat_names[i]].append(
+                    attr[:, 1, i][mask[:, i]].detach().numpy()
+                )
+
+        for i in range(1000):
+            imp[self.feat_names[i]] = np.concatenate(imp[self.feat_names[i]])
+
+        return imp
+
+    def attr_snps_mean(self, attrs, mask):
+        imp = {}
+        for i in range(1000):
+            imp[self.feat_names[i]] = np.concatenate(
+                [attr[:, 1, i][mask[:, i]].detach().numpy() for attr in attrs]
+            )
+
+        return imp
